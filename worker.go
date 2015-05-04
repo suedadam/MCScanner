4dc2cb84fab11ff6e725a52e1cc9b36d225d8ec7
Connor Peet <connor@peet.io>
2015-05-04 18:23:58 -0400

    A bit more graceful async

--- worker.go
+++ worker.go
@@ -7,13 +7,14 @@ import (
 type pool struct {
 	checks  chan string
 	workers int
-	endings []chan bool
+	ending  chan bool
 }
 
 func newPool(workers int) *pool {
 	p := &pool{
 		// Buffer the channel with 10x as many IPs as workers.
 		checks:  make(chan string, workers*10),
+		ending:  make(chan bool, workers),
 		workers: workers,
 	}
 	p.spawn()
@@ -24,9 +25,7 @@ func newPool(workers int) *pool {
 // Spawns out the number of workers in the pool.
 func (p *pool) spawn() {
 	for i := 0; i < p.workers; i++ {
-		end := make(chan bool)
-		p.endings = append(p.endings, end)
-		go p.work(end)
+		go p.work()
 	}
 }
 
@@ -35,25 +34,23 @@ func (p *pool) add(ip string) {
 	p.checks <- ip
 }
 
-// Listens from and works on the checks until the "end" signal is
-// sent.
-func (p *pool) work(end chan bool) {
-	for {
-		select {
-		case ip := <-p.checks:
-			if isMinecraft(ip) {
-				fmt.Println(ip)
-			}
-		case <-end:
-			return
+// Listens from and works on the checks.
+func (p *pool) work() {
+	for ip := range p.checks {
+		if isMinecraft(ip) {
+			fmt.Println(ip)
 		}
 	}
+
+	p.ending <- true
 }
 
 // Closes the pool. Should be sent when you're done adding IPs to
-// be checked. Blocks until the pool is finished working.
+// be checked. Blocks until the pool is finished.
 func (p *pool) end() {
-	for _, end := range p.endings {
-		end <- true
+	close(p.checks)
+
+	for i := 0; i < p.workers; i++ {
+		<-p.ending
 	}
 }
